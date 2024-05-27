ca1e1f52171443f6e533c3cd4fd0044503d20dd2
nisha <nishasingla05@gmail.com>
2024-05-27 17:03:09 +0530

    Recent

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log('First Commit');
 console.log('Second Commit');
 console.log('Third Commit');
+console.log('Recent Commit');
+
+
+// git log
+// Git log command display commited snapshots.

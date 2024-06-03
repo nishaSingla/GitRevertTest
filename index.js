d6199f654b0f2c4cbf12e87875c96cfd129f8670
nisha <nishasingla05@gmail.com>
2024-06-03 18:18:38 +0530

    remove

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 console.log('First Commit');
 console.log('Second Commit');
 console.log('Third Commit');
-console.log('Recent Commit');
 
 
-// git log
-// Git log command display commited snapshots.
